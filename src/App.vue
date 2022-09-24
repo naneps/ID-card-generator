925c1338a7be85372fd05035b250a547d288e8ac
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-24 21:41:45 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template >
+  <div class="bg-black h-screen flex justify-center items-center">
+    <h1 class="text-3xl text-yellow-100 font-bold ">
+    Hello world!
+  </h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style >
+  
+</style>
